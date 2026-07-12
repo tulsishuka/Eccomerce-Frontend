b35c0025fa3664dbeec103a4f7beb88e7d848b8a
Tulasi <tulsishuklag@gmail.com>
2026-07-12 14:22:03 +0530

    frontend/src/pages/FlashSales/Product.jsx

--- frontend/src/pages/FlashSales/Product.jsx
+++ frontend/src/pages/FlashSales/Product.jsx
@@ -1,16 +1,4 @@
-// import FlashSales from "../../components/FlashSales/FlashSales"
-// import ProductSection from "../../components/FlashSales/ProductCard"
 
-// const Product = () => {
-//   return (
-//   <>
-//   <FlashSales/>
-//   <ProductSection/>
-//   </>
-//   )
-// }
-
-// export default Product
 
 import  { useRef } from 'react'
 import FlashSales from "../../components/FlashSales/FlashSales"
@@ -21,7 +9,6 @@ const Product = () => {
 
   const scrollLeft = () => {
     if (sliderRef.current) {
-      // Scrolls left by the width of roughly one or two items
       sliderRef.current.scrollBy({ left: -300, behavior: 'smooth' });
     }
   };
